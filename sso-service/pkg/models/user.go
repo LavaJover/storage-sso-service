942b8e0a483577daef4f7cebc889fb83d0f263d8
bodya <lavajover@gmail.com>
2025-02-08 01:24:24 +0300

    feat: add user model

---
+++ sso-service/pkg/models/user.go
@@ -0,0 +1,10 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct{
+	gorm.Model
+	Email string `gorm:"column:email;type:varchar;size:255;unique"`
+	Password string `gorm:"column:password;type:varchar;size:255"`
+	IsActive bool `gorm:"column:is_active;type:tinyint"`
+}
