916392629fda6a4a1946915ec3f99b71f44564aa
bodya <lavajover@gmail.com>
2025-02-08 18:47:22 +0300

    feat: add find user by email method

--- sso-service/internal/repository/user_repo.go
+++ sso-service/internal/repository/user_repo.go
@@ -1,6 +1,8 @@
 package repo
 
 import (
+	"log/slog"
+
 	"github.com/LavaJover/storage-sso-service/sso-service/pkg/models"
 	"gorm.io/gorm"
 )
@@ -18,4 +20,16 @@ func (repo *UserRepo) CreateUser (newUser *models.User) error{
 	}
 
 	return nil
+}
+
+// Get user from database by email
+func (repo *UserRepo) GetUserByEmail (email string) (*models.User, error){
+	user := &models.User{}
+	result := repo.DB.Where("email = ?", email).First(user)
+
+	if result.Error != nil{
+		slog.Error("failed to find user by email", "msg", result.Error.Error())
+		return nil, result.Error
+	}
+	return user, nil
 }
