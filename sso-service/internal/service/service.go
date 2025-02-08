16fc0715e620855b7319ab20d1423859ebe32d3e
bodya <lavajover@gmail.com>
2025-02-08 18:47:42 +0300

    feat: add create user method in service layer

--- sso-service/internal/service/service.go
+++ sso-service/internal/service/service.go
@@ -1,1 +1,14 @@
 package servie
+
+import (
+	repo "github.com/LavaJover/storage-sso-service/sso-service/internal/repository"
+	"github.com/LavaJover/storage-sso-service/sso-service/pkg/models"
+)
+
+type SSOService struct{
+	*repo.UserRepo
+}
+
+func (service *SSOService) CreateUser (newUser *models.User) error{
+	return service.UserRepo.CreateUser(newUser)
+}
